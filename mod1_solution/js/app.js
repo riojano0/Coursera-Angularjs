213ea1a8ad5bf57852d035c430ae05c52832b5dd
riojano0 <danielelnuevo@hotmail.com>
2016-12-23 01:19:51 -0300

    First Asigment fix color

--- mod1_solution/js/app.js
+++ mod1_solution/js/app.js
@@ -9,6 +9,7 @@ angular.module('LunchCheck',[])
 
 		$scope.alertMessage = "";
 		$scope.lunchList = "";
+		$scope.colorMessage= "green";
 
 		$scope.checkList = function(){
 			var arrayLunch=$scope.lunchList.split(",")
@@ -23,12 +24,16 @@ angular.module('LunchCheck',[])
 		$scope.alertChecker = function(itemsCount){
 			if (itemsCount>3){
 				$scope.alertMessage= "Too Much!";
+				$scope.colorMessage= "green";
 			}
 			else if(itemsCount>0){
 				$scope.alertMessage= "Enjoy!";	
+				$scope.colorMessage= "green";
+
 			}
 			else{
 				$scope.alertMessage= "Please enter data first";	
+				$scope.colorMessage= "red";
 			}
 		}
 
